0a50f3f26a235295e72baa44d46b20792ab1264f
longnguyendev <thanhlong210494@gmail.com>
2024-05-31 13:49:27 +0700

    edit code

--- movie/data.php
+++ movie/data.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once('./config/database.php');
+// require_once('./config/database.php');
 // autoloading
 // spl_autoload_register(function ($className) {
 //     require_once("./models/$className.php");

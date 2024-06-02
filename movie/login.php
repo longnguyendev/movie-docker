d5bcd674f972cb5e2764a472aa832c70e8a2fd8c
longnguyendev <thanhlong210494@gmail.com>
2024-06-02 15:05:05 +0700

    fix login

--- movie/login.php
+++ movie/login.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
 include 'data.php';
 $notication = "";
 if (isset($_SESSION['user']['name'])) {
